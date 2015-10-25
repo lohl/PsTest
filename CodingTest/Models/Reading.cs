24810d6b26799763b42c933c0e6506e63d605484
Simon Timms <stimms@gmail.com>
2015-10-25 20:02:46 +0000

    Initial Add

---
+++ CodingTest/Models/Reading.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodingTest.Models
+{
+    public class Reading
+    {
+        public int Id { get; set; }
+        public decimal Depth { get; set; }
+        [Range(0, 10)]
+        public decimal MagX { get; set; }
+        [Range(0, 10)]
+        public decimal MagY { get; set; }
+        [Range(0, 10)]
+        public decimal MaxZ { get; set; }
+        [NotMapped]
+        public decimal TotalMag { get
+            {
+                return MagX + MagY + MaxZ;
+            }
+        }
+    }
+}
